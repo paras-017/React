389089b13fbc73500f10612c1c45039adb4f5a05
paras-017 <paraschauhan0017@gmail.com>
2023-10-16 11:07:36 +0530

    UI done

--- passGen/src/App.jsx
+++ passGen/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -9,11 +8,45 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [Password, setPassword] = useState('')
 
-  const 
+  const passwordGenerator = useCallback(()=>{
+    const pass = ""
+    const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*(){}~"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+    }
+    setPassword(pass)
+  }, [length,numberAllowed,charAllowed,setPassword])
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Password Generator</h1>
+    <div className=" text-center w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500">
+      <h1 className="text-white text-center">Password Generator</h1>
+      <div className="flex shadow overflow-hidden mb-4">
+        <input type="text" value={Password} className='outline-none w-full py-1 px-3 my-3' placeholder='password' readOnly/>
+        <button className='outline-none bg-blue-700 text-white px-3 shrink-0'>copy</button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className='flex items-center gap-x-1'>
+          <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={e=>{setLength(e.target.length)}}/>
+          <label>Lenth:{length}</label>
+        </div>
+        
+        <div className='flex items-center gap-x-1'>
+          <input id='numberInput' type="checkbox" defaultChecked={numberAllowed} onChange={()=>{setNumberAllowed((prev)=>!prev)}}/>
+          <label htmlFor="numberInput">Number</label>
+        </div>
+        
+        <div className='flex items-center gap-x-1'>
+          <input id='charInput' type="checkbox" defaultChecked={charAllowed} onChange={()=>{setCharAllowed((prev)=>!prev)}}/>
+          <label htmlFor="charInput">character</label>
+        </div>
+
+      </div>
+    </div>
     </>
   )
 }
