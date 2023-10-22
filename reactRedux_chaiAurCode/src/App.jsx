4cec84d822c85bb29e95bdea013a1c909b3dd3fe
paras-017 <paraschauhan0017@gmail.com>
2023-10-22 19:43:19 +0530

    completed

--- reactRedux_chaiAurCode/src/App.jsx
+++ reactRedux_chaiAurCode/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1>Learn about Redux Toolkit with Chai Aur Code</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
   )
 }
 
